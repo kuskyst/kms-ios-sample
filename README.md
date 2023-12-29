965a865650ea35a2e783df5d470c58427e0ef8f1
kohsaka <>
2023-12-29 10:14:12 +0900

    first

---
+++ README.md
@@ -0,0 +1,5 @@
+# kms-ios-sample
+CloudHSM経由のKMSから秘密鍵を取得して、暗号/復号化するサンプルコード
+
+# Image
+<img width=150 src="https://github.com/kuskyst/kms-ios-sample/assets/126965999/d9acd3ae-2f5d-44d1-8774-f0aa96404493">
---
+++ kms-ios-sample/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  
+//  
+//  Created by kohsaka on 2023/12/28
+//  
+//
+
+import UIKit
+import AWSKMS
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var encrypt: UITextField!
+    @IBOutlet private weak var encryptResult: UITextField!
+    @IBOutlet private weak var decrypt: UITextField!
+    @IBOutlet private weak var decryptResult: UITextField!
+
+    private var client: AWSKMS?
+    private let accessKey = "please set accessKey"
+    private let secretKey = "please set secretKey"
+    private let keyId = "please set keyId"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let provider = AWSStaticCredentialsProvider(
+            accessKey: self.accessKey, secretKey: self.secretKey)
+        let configuration = AWSServiceConfiguration(
+            region: AWSRegionType.APNortheast1,
+            credentialsProvider: provider)
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        self.client = AWSKMS.default()
+    }
+
+    @IBAction private func encryptTapped() {
+        if (!self.encrypt.text!.isEmpty) {
+            let request = AWSKMSEncryptRequest()
+            request?.plaintext = self.encrypt.text!.data(using: .utf8)
+            request?.keyId = self.keyId
+            self.client!.encrypt(request!).continueWith { (task: AWSTask<AWSKMSEncryptResponse>) -> Any? in
+                DispatchQueue.main.async {
+                    self.encryptResult.text = task.result!.ciphertextBlob!.base64EncodedString()
+                }
+                return
+            } .waitUntilFinished()
+        }
+    }
+
+    @IBAction private func decryptTapped() {
+        if (!self.decrypt.text!.isEmpty) {
+            let request = AWSKMSDecryptRequest()
+            request?.ciphertextBlob = Data(base64Encoded: self.decrypt.text!.data(using: .utf8)!)
+            request?.keyId = self.keyId
+            self.client!.decrypt(request!).continueWith { (task: AWSTask<AWSKMSDecryptResponse>) -> Any? in
+                DispatchQueue.main.async {
+                    self.decryptResult.text = String(data: (task.result!.plaintext)!, encoding: .utf8)
+                }
+                return
+            } .waitUntilFinished()
+        }
+    }
+}
+
